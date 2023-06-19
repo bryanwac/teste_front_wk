d8dd6ec194af77f1b53064f1c9d0d0feb902c158
bryanwac <bryanwac@gmail.com>
2023-06-19 09:41:05 -0300

    feat: responsividade adicionada. feat: estilização de componentes melhoradas. feat: função logout na navbar.

--- src/app/navegacao/navegacao.component.ts
+++ src/app/navegacao/navegacao.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { TokenStorageService } from '../services/token-storage.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalAlertasComponent } from '../util/modal-module/modal-alertas/modal-alertas.component';
 
 @Component({
   selector: 'app-navegacao',
@@ -7,11 +10,17 @@ import { NavigationEnd, Router } from '@angular/router';
   styleUrls: ['./navegacao.component.scss']
 })
 export class NavegacaoComponent implements OnInit {
+  
   sidebarVisible = false;
   selectedItem: string | undefined;
   shouldShowNavigation = true;
+  isMobileViewport: boolean;
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private tokenService: TokenStorageService,
+    private dialog: MatDialog,
+    ) { }
 
   ngOnInit(): void {
     this.router.events.subscribe((event) => {
@@ -28,5 +37,27 @@ export class NavegacaoComponent implements OnInit {
   selectItem(item: string): void {
     this.selectedItem = item;
     this.sidebarVisible = !this.sidebarVisible;
+  }
+
+  logout(): void {
+    const data = {
+      name: 'Alerta de logout',
+      mensagem: 'Você será redirecionado para a tela de login.'
+    }
+    this.dialog.open(ModalAlertasComponent, { data })
+    .afterClosed()
+    .subscribe((_) => {
+      this.tokenService.signOut();
+    });
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event: any) {
+    this.isMobileViewport = this.checkIfMobileViewport();
+  }
+
+  checkIfMobileViewport(): boolean {
+    const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    return width < 768; // Ajuste o valor conforme necessário
   }
 }
